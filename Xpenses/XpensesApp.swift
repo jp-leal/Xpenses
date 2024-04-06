00d65bc62635ce359aaaf0a3814e49f880c4b038
joaobug <joaoenfrance@gmail.com>
2024-04-06 14:07:58 -0300

    Initial Commit

---
+++ Xpenses/XpensesApp.swift
@@ -0,0 +1,17 @@
+//
+//  XpensesApp.swift
+//  Xpenses
+//
+//  Created by Joao Leal on 4/6/24.
+//
+
+import SwiftUI
+
+@main
+struct XpensesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
